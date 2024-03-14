99db84cfbf3d85affd6d600c904bf74908882e95
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2024-03-14 23:57:06 +0700

    feat: adding kinde. now can sign in

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,43 @@
+import { kindeAuthClient, type SessionManager } from '@kinde-oss/kinde-auth-sveltekit';
+import type { RequestEvent } from '@sveltejs/kit';
+
+export async function load({ request }: RequestEvent) {
+	const isAuthentication = await kindeAuthClient.isAuthenticated(
+		request as unknown as SessionManager
+	);
+	let userProfile = null;
+	if (isAuthentication) {
+		userProfile = await kindeAuthClient.getUser(request as unknown as SessionManager);
+
+		const userOrganizations = await kindeAuthClient.getUserOrganizations(
+			request as unknown as SessionManager
+		);
+		const permission = await kindeAuthClient.getPermission(
+			request as unknown as SessionManager,
+			'read:profile'
+		);
+		const permissions = await kindeAuthClient.getPermissions(request as unknown as SessionManager);
+		const aud = await kindeAuthClient.getClaim(request as unknown as SessionManager, 'aud');
+
+		console.log({
+			isAuthentication,
+			userProfile,
+			userOrganizations,
+			permission,
+			permissions,
+			aud
+		});
+	}
+
+	// const config = await getConfiguration();
+
+	// const apiInstance = new UsersApi(config);
+
+	// const users = await apiInstance.getUsers();
+	// console.log('users', users);
+
+	return {
+		isAuthentication,
+		userProfile
+	};
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Header from '$lib/Header.svelte';
+	export let data;
+</script>
+
+<svelte:head>
+	<title>Kinde Auth | SvelteKit Starter Kit</title>
+</svelte:head>
+
+<Header isAuthenticated={data.isAuthentication} user={data.userProfile} />
