fa4f80c61ad6806b7d1d7d5b7368645590f61627
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2024-03-27 11:34:04 +0700

    feat: add image preview. todo: make it a component, handle multple file at once

--- src/routes/(home)/photos/+page.svelte
+++ src/routes/(home)/photos/+page.svelte
@@ -4,7 +4,9 @@
 	import Button from '$ui/button.svelte';
 	import axios from 'axios';
 	let files: FileList;
-
+	let showImage: boolean;
+	let image: any;
+	let placeholder: any;
 	$: if (files) {
 		// Note that `files` is of type `FileList`, not an Array:
 		// https://developer.mozilla.org/en-US/docs/Web/API/FileList
@@ -19,10 +21,34 @@
 		const body = new FormData();
 		body.append('image', files[0]);
 
-		const result = await fetch(`https://api.imgbb.com/1/upload?key=${env.PUBLIC_IMGBB_API_KEY}`, {
-			method: 'POST',
-			body
-		}).then((result) => result.json());
+		// create axios request with onProgress
+		const axiosRequest = await axios.post(
+			`https://api.imgbb.com/1/upload?key=${env.PUBLIC_IMGBB_API_KEY}`,
+			body,
+			{
+				onUploadProgress: (progressEvent) => {
+					console.log(progressEvent.progress);
+				}
+			}
+		);
+		console.log(axiosRequest);
+	}
+
+	function onChange() {
+		const file = files[0];
+
+		if (file) {
+			showImage = true;
+
+			const reader = new FileReader();
+			reader.addEventListener('load', function () {
+				image.setAttribute('src', reader.result);
+			});
+			reader.readAsDataURL(file);
+
+			return;
+		}
+		showImage = false;
 	}
 </script>
 
@@ -30,7 +56,7 @@
 	<div class="text-3xl">Photos</div>
 	<!-- input file upload -->
 	<label for="many">Upload multiple files of any type:</label>
-	<input bind:files id="many" multiple type="file" />
+	<input bind:files id="many" multiple type="file" on:change={onChange} />
 	{#if files}
 		<h2>Selected files:</h2>
 		{#each Array.from(files) as file}
@@ -39,5 +65,10 @@
 		<div>
 			<Button on:click={handleClick}>upload!</Button>
 		</div>
+		{#if showImage}
+			<img bind:this={image} src="" alt="Preview" class="w-36" />
+		{:else}
+			<span bind:this={placeholder}>Image Preview</span>
+		{/if}
 	{/if}
 </div>
